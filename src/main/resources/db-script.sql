21295e294ffa2d49d484d26e3e4120f274ed8fd6
Chathura Madhusanka <madhusanka.slc@gmail.com>
2022-11-29 11:49:23 +0530

    create todo_item table

--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -3,3 +3,11 @@ CREATE TABLE user(
     full_name VARCHAR(300) NOT NULL ,
     password VARCHAR(50) NOT NULL
 );
+
+CREATE  TABLE  todo_item(
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    user_name VARCHAR(200) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    status ENUM('DONE','NOTDONE') NOT NULL,
+    CONSTRAINT FOREIGN KEY (user_name) REFERENCES user(user_name)
+)
