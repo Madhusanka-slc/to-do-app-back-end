36dd7555312ce8894defb7d52b216b3139c6bda7
Chathura Madhusanka <madhusanka.slc@gmail.com>
2022-11-29 16:43:29 +0530

    fix dao factory

--- src/main/java/lk/ijse/dep9/dao/custom/impl/QueryDAOImpl.java
+++ src/main/java/lk/ijse/dep9/dao/custom/impl/QueryDAOImpl.java
@@ -2,5 +2,12 @@ package lk.ijse.dep9.dao.custom.impl;
 
 import lk.ijse.dep9.dao.custom.QueryDAO;
 
+import java.sql.Connection;
+
 public class QueryDAOImpl implements QueryDAO {
+    private Connection connection;
+
+    public QueryDAOImpl(Connection connection) {
+        this.connection = connection;
+    }
 }
